b717248a69d0e83a19201e35912becb67cd1724c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-03-22 11:10:02 +0100

    Cleanup Unit Tests

---
+++ ContestProjectTests/ContestTest.cs
@@ -0,0 +1,32 @@
+﻿using ContestProjectWPF;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContestProjectTests
+{
+    [TestClass]
+    public class ContestTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var c = new Code();
+
+            var input = @"";
+
+            var result = c.Execute(input);
+            result.Should().Be(@"");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var c = new Code();
+
+            var input = @"";
+
+            var result = c.Execute(input);
+            result.Should().Be(@"");
+        }
+    }
+}
